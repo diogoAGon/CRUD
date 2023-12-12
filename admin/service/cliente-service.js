e81808b8f0ca245bdb1c19bbd5362b2ed831132b
devdiogo1 <dev1diogo@outlook.com>
2023-12-12 17:47:19 -0300

    Refatorando codigo com promises para melhorar a leitura

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -19,19 +19,33 @@ return linhaNovoCliente
 
 const tabela = document.querySelector ('[data-tabela]')
 
-const http = new XMLHttpRequest()   
+const listaClientes = () => {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest()   
 
-http.open('GET', 'http://localhost:3000/profile')
-
-http.send()
-
-http.onload = () => {
-    // transformando essa resposta em um objeto javascript, p conseguir ter acesso.
-    const data = JSON.parse(http.response)
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-    })
-
-   
+        http.open('GET', 'http://localhost:3000/profile')
+        
+       
+        
+        http.onload = () => {
+            if (http.status >= 400){
+                reject(JSON.parse(http.response))
+        
+            }else {
+                resolve(JSON.parse(http.response))
+            }
+            }
+            http.send()
+        })
+            return promise
+    
 
 }
+
+ listaClientes()
+ .then( data =>  {  
+    data.forEach(elemento => {
+    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+   
+   
+   })})
